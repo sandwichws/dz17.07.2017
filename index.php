1ddf190a7636983cda7c3af4726ff706c458a23c
Andrew Babich <dearsa2015@mail.ru>
2017-08-07 16:34:30 +0300

    second commit, fix everything

--- index.php
+++ index.php
@@ -6,39 +6,51 @@
 <link href="css/style_mq.css" rel="stylesheet">
 <link href="css/style_mq2.css" rel="stylesheet">
 
+
 </head>
 <body>
-  <div class="header" align="center">
+  <div id="header" align="center">
     <h1 class="textHead"><b>РАКЕТКА <img class="logoEdit" src ="logo.png"></b></h1>
-  </div>
-  <div class="menu"><h3 align="center" class="textMar">Главная</h3></div>
-  <div class="menu"><h4 align="center" class="textMar">Вопросы и ответы</h4></div>
-  <div class="menu"><h4 align="center" class="textMar">Доставка и оплата</h4></div>
-   <div class="menu"><h3 align="center" class="textMar">Корзина</h3></div>
-   <div class="menu"><h3 align="center" class="textMar">Контакты</h3></div>
+</div>
 
-   <div class="main">
+  <div class="menuBL" align="center">
+  <div id="menu" class="textMar"><b>Главная</b></div>
+  <div id="menu" class="textMLong"><b>Вопросы и ответы</b></div>
+  <div id="menu" class="textMLong"><b>Доставка и оплата</b></div>
+  <div id="menu" class="textMar"><b>Корзина</b></div>
+  <div id="menu" class="textMar"><b>Контакты</b></div>
+</div>
+
+<div align="center">
+
+     <div align="center" id="content">
        <?php
          $price = rand(500, 1500);
          $count = 30;
          while($count > 0): ?>
 
-         <div class="bords"><img class="imgMod" src="images/<?php echo $count;?>.jpg">
-          <h6 class="descr">Ракетка <br>такая-то</h6>
+         <div class="bords" align="center"><img class="imgMod" src="images/<?php echo $count;?>.jpg">
+          <h6>Ракетка <br>такая-то</h6>
           <b class="pric"><?php echo rand(500, 1500), "грн </br>"; ?></b>
         </div>
          <?php $count -= 1; ?>
        <?php endwhile; ?>
+     </div>
 
-       <div>
-         <img class="banr" src="banr.png">
-       </div>
+     <div id="sidebar">
+        <a><img class="banr" src="banr.png"></a>
+     </div>
 
-   </div>
 
-   <div class="footer" align="center">
-     <h4 class="textFoot"><b>РАКЕТКА 2017 &copy</b></h4>
-   </div>
+</div>
+
+
+<div align="center" id="footer">
+   <b>РАКЕТКА 2017 &copy</b>
+</div>
+
+
+
 
   </body>
   <html>
